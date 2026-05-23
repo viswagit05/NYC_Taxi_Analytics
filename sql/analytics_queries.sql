dc7bf7dc09ba66af426a13c6705d14f21304019f
VISWA B <gvish2005@gmail.com>
2026-05-23 23:49:21 +0530

    Add files via upload

---
+++ sql/analytics_queries.sql
@@ -0,0 +1,11 @@
+-- Final analytical queries for NYC taxi analytics
+
+-- Example query placeholder
+SELECT
+    pickup_datetime,
+    dropoff_datetime,
+    passenger_count,
+    trip_distance,
+    fare_amount
+FROM nyc_taxi_trips
+LIMIT 100;
